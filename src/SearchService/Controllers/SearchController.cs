e9f68e9ea43f45820f83dea3ea1ca4ef2ffd11a4
Pranav kumar <prnvkmr954@gmail.com>
2025-09-03 16:17:40 +0530

    feat: added new controllrt and api endpoint search term

---
+++ src/SearchService/Controllers/SearchController.cs
@@ -0,0 +1,27 @@
+using System;
+using Microsoft.AspNetCore.Mvc;
+using MongoDB.Entities;
+using SearchService.Models;
+
+namespace SearchService.Controllers;
+
+[ApiController]
+[Route("api/search")] 
+public class SearchController : ControllerBase
+{
+    public async Task<ActionResult<List<Item>>> SearchItem(string searchTerm)
+    {
+        var query = DB.Find<Item>();
+
+        query.Sort(x => x.Ascending(a => a.Make));
+
+        if (!string.IsNullOrEmpty(searchTerm))
+        {
+            query.Match(Search.Full, searchTerm).SortByTextScore();
+        }
+
+        var result = await query.ExecuteAsync();
+
+        return result;
+    }
+}
