52ba7acd59d43c6a68cdd0fb27d30cb670187dca
Pranav kumar <prnvkmr954@gmail.com>
2025-09-03 15:29:36 +0530

    feat: added db configuration and setup program.cs for search service

--- src/SearchService/Program.cs
+++ src/SearchService/Program.cs
@@ -1,7 +1,11 @@
+using MongoDB.Driver;
+using MongoDB.Entities;
+using SearchService.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddControllers();
 
 var app = builder.Build();
 
@@ -11,8 +15,17 @@ if (app.Environment.IsDevelopment())
 
 }
 
-app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
 
+await DB.InitAsync("SearchDb",MongoClientSettings.
+                FromConnectionString(builder.Configuration.GetConnectionString("MongoDbConnection")));
+
+await DB.Index<Item>()
+        .Key(x => x.Make, KeyType.Text)
+        .Key(x => x.Model, KeyType.Text)
+        .Key(x => x.Color, KeyType.Text)
+        .CreateAsync();
 
 app.Run();
 
