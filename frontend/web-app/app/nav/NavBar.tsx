958d830b4fb34aa75689c0d92175b2cd6a52e41d
Pranav kumar <prnvkmr954@gmail.com>
2025-09-24 00:54:28 +0530

    feat: added client side auth

--- frontend/web-app/app/nav/NavBar.tsx
+++ frontend/web-app/app/nav/NavBar.tsx
@@ -1,3 +1,4 @@
+import LoginButton from "./LoginButton";
 import Logo from "./Logo";
 import Search from "./Search";
 
@@ -7,7 +8,7 @@ export default function NavBar() {
     <header className="sticky top-0 z-50 flex justify-between bg-white p-5 items-center text-gray-800 shadow-md">
         <Logo />
         <Search />
-        <div>Login</div>
+        <LoginButton />
     </header>
   )
 }
